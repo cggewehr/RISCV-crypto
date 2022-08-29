e6f4342d3e073d101a4f266cc754cb47e5a6a50b
Carlos <cggewehr@gmail.com>
2022-08-28 23:38:20 -0300

    Makefile spaces to tabs

--- scripts/sim_common.mk
+++ scripts/sim_common.mk
@@ -2,49 +2,49 @@
 
 # Set tool-specific variables
 ifeq VENDOR Cadence
-    COMP_EXEC=ncvlog
-    COMP_OPTS=-logfile log/cadence/ncvlog.log -errormax 15 -update -linedebug -status -sv -work worklib
-    ELAB_EXEC=ncelab
-    ELAB_OPTS=-logfile log/cadence/ncelab.log -errormax 15 -update -status -defparam tb_top.SRAMInitFile=${SIM_PATH}/MemFile.vmem worklib.tb_top
-    SIM_EXEC=ncsim
-    SIM_OPTS=-logfile log/cadence/ncsim.log -errormax 15
-    SIM_GUI_OPTS=${SIM_OPTS} -gui
+	COMP_EXEC=ncvlog
+	COMP_OPTS=-logfile log/cadence/ncvlog.log -errormax 15 -update -linedebug -status -sv -work worklib
+	ELAB_EXEC=ncelab
+	ELAB_OPTS=-logfile log/cadence/ncelab.log -errormax 15 -update -status -defparam tb_top.SRAMInitFile=${SIM_PATH}/MemFile.vmem worklib.tb_top
+	SIM_EXEC=ncsim
+	SIM_OPTS=-logfile log/cadence/ncsim.log -errormax 15
+	SIM_GUI_OPTS=${SIM_OPTS} -gui
 
 else
-    @echo Vendor <${VENDOR}> not supported by this makefile
-    exit 1
-    
+	@echo Vendor <${VENDOR}> not supported by this makefile
+	exit 1
+	
 sw:
 
-    @echo ---- Building VMEM file from ${PROGRAM} dir
-    @export
-    make -f ${ROOT_PATH}/sw/${PROGRAM}/Makefile
-    @echo ---- Done building VMEM file from ${PROGRAM} dir
+	@echo ---- Building VMEM file from ${PROGRAM} dir
+	@export
+	make -f ${ROOT_PATH}/sw/${PROGRAM}/Makefile
+	@echo ---- Done building VMEM file from ${PROGRAM} dir
 
 compile:
 
-    ${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_RTL}/${RISCV_CORE}/${RISCV_CORE}.f
-    ${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_RTL}/util/util.f
-    ${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_RTL}/simple-system-top/ibex_simple_system.sv
-    ${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_TBENCH}/tb.f
+	${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_RTL}/${RISCV_CORE}/${RISCV_CORE}.f
+	${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_RTL}/util/util.f
+	${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_RTL}/simple-system-top/ibex_simple_system.sv
+	${COMP_EXEC} ${COMP_OPTS} ${RISCV_CRYPTO_TBENCH}/tb.f
 
 elab:
 
-    ${ELAB_EXEC} ${ELAB_OPTS}
+	${ELAB_EXEC} ${ELAB_OPTS}
 
 sim:
 
-    ${SIM_EXEC} ${SIM_OPTS}
+	${SIM_EXEC} ${SIM_OPTS}
 
 simgui:
 
-    ${SIM_EXEC} ${SIM_GUI_OPTS}
+	${SIM_EXEC} ${SIM_GUI_OPTS}
 
 all: compile elab sim
-    
+	
 allgui: compile elab simgui
-    
+	
 clean:
 
-    rm xcelium.d
-    rm log
+	rm xcelium.d
+	rm log
