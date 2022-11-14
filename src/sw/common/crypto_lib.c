d3271106d523f4c0119b29cb17b1b4e6752e68aa
Carlos <cggewehr@gmail.com>
2022-11-14 13:18:44 -0300

    Fixes W[i] pointers

--- src/sw/common/crypto_lib.c
+++ src/sw/common/crypto_lib.c
@@ -16,7 +16,7 @@ const unsigned int k256[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0
                                0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070
                                0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3
                                0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
-                      
+
 // SHA512 initial values
 const unsigned long long h512[8] = {0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
                                     0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179};
@@ -98,7 +98,7 @@ void sha256_compress(char* state, char* data) {
     // a1: Pointer to message block
     // a2: throwaway temp (storeStateFlag argument is saved to t3)
     // a3: Data from compressed load/store & throwaway temp
-    // a4: Pointer to sha2_workspace[] (Used for compressed load/store) 
+    // a4: Pointer to sha2_workspace[] (Used for compressed load/store)
     // a5: throwaway temp
 
     // s0: H state variable
@@ -120,23 +120,22 @@ void sha256_compress(char* state, char* data) {
     // a7: A running state
 
     // t0: i counter
-    // t2: Saves SP, restores at the end (saved in sha256_init(), restored in sha2_restore())
+    // t2: Saves SP, restores at the end
     // t4: workspace index mask (0xF)
     // t5: Pointer to sha2_workspace[] (Used for compressed load/store)
     // t6: (A and B) from previous iteration, used as (B and C) in next iteration
 
     asm (
 
-        // Init t0 to round counter (0 < i < 64) and t1 to workspace index (0 < i < 16)
+        // Init t0 to round counter (0 < i < 64)
         c.mv t0, x0
-        c.mv t1, x0
-        c.mv t3, a2
-        c.li t4, 16
+        c.mv t2, a2
+        li t4, 0x3f
 
         la sp, k256
         la a4, workspace
         c.mv t5, a4
-        
+
         // Init state variables from running hash value
         c.mv s0, t3
         c.mv s1, t1
@@ -146,50 +145,51 @@ void sha256_compress(char* state, char* data) {
         c.mv s5, s11
         c.mv s6, a6
         c.mv s7, a7
-        
+
         // t6 <= (A and B) from first iteration
         and t6, a6, a7
-        
+
         sha256_compress_iter_top:
-            
+
             // Choose if load W[i] from data[] (i < 16) or compute new W (i >= 16)
-            bge t0, t4, sha256_compress_compute_new_W
-            
+            li a3, 64  // (16 << 2)
+            bge t0, a3, sha256_compress_compute_new_W
+
             // Fall through to load W[i] from data[], i < 16
             sha256_compress_load_W_from_workspace:
-                
+
                 // a2 <= data[i]
                 c.lw a2, a1, 0
-                
+
                 // Convert data[i] to big-endian
                 li a3, 0xFF
-                
+
                 and a5, a3, a2
                 c.slli a5, 24
                 c.slli a3, 8
                 c.or a4, a5
-                
+
                 and a5, a3, a2
                 c.slli a5, 8
                 c.slli a3, 8
                 c.or a4, a5
-                
+
                 and a5, a3, a2
                 c.srli a5, 8
                 c.slli a3, 8
                 c.or a4, a4, a5
-                
+
                 and a5, a3, a2
                 c.srli a5, 25
                 c.or a4, a5
-                
+
                 // W[i] <= data[i]
                 c.mv a4, t5
                 c.sw a2, a4, 0
-                
+
                 // Increment pointer to data[i]
                 c.addi a1, 4
-        
+
                 // Jump to "sha256_compress_compute_state"
                 c.j sha256_compress_compute_state
 
@@ -198,40 +198,40 @@ void sha256_compress(char* state, char* data) {
 
                 // Get a2 = W[i+1 & 0xF] (equivalent to W[i-15])
                 c.mv t0, a5
-                c.addi a5, 1
+                c.addi a5, 4  // (1 << 2)
                 c.andi a5, 0xF
                 c.add a5, a4
                 c.lw a2, a5
 
                 // Compute Sigma0(a2)   W[i+1]
                 sha256sig0 a2, a2
-                
+
                 // Get a3 = W[i+14 & 0xF] (equivalent to W[i-2])
                 c.mv t0, a5
-                c.addi a5, 14
-                c.andi a5, 0xF
+                c.addi a5, 56  // (14 << 2)
+                c.and a5, t4
                 c.add a5, a4
                 c.lw a3, a5
-                
+
                 // Compute Sigma1(a3)   W[i+14]
                 sha256sig1 a3, a3
-                
+
                 // a2 = a2 + a3 (a2 now contains Sig0(W[i-2]) + Sig1(W[i-14]))
                 c.add a2, a3
-                
+
                 // Get a5 = W[i] (will be replaced in this iteration)  (equivalent to W[i-16])
                 c.lw a3, a4
-                
+
                 // a2 = a2 + a3  (a2 now contains Sig0(W[i-2]) + Sig1(W[i-14] + W[i-16]))
                 c.add a2, a3
 
                 // Get a3 = W[i+9 & 0xF]  (equivalent to W[i-7])
                 c.mv t0, a5
-                c.addi a5, 14
-                c.andi a5, 0xF
+                c.addi a5, 36  // (9 << 2)
+                c.and a5, t4
                 c.add a5, a4
                 c.lw a3, a5
-                
+
                 // a2 = a2 + a3  (a2 now contains Sig0(W[i-2]) + Sig1(W[i-14] + W[i-16] + W[i-14], new W[i] is finished being computed))
                 c.add a2, a3
 
@@ -243,16 +243,16 @@ void sha256_compress(char* state, char* data) {
 
                 sha256sum1 a5, s3  // a5 <= Sum1(E)
                 c.add a2, a5  // a2 not contains W[i] + Sum1(E)
-                
-                c.lwsp a5
+
+                c.lwsp a5, 0
                 c.add a2, a5  // a2 not contains W[i]) + Sum1(E) + K[i]
                 c.add a2, s0  // a2 not contains W[i]) + Sum1(E) + K[i] + H
-                
+
                 // Updates H, G, and F variables, freeing up s4 (current E) register for temp use
                 c.mv s0, s1  // s0 now contains G
                 c.mv s1, s2  // s1 now contains F
                 c.mv s2, s3  // s2 now contains E
-                
+
                 // Compute CH(E, F, G)
                 and a5, s1, s2
                 xori s4, s4, -1
@@ -261,7 +261,7 @@ void sha256_compress(char* state, char* data) {
                 c.add a2, a5  // a2 not contains W[i] + Sum1(E) + K[i] + CH(E, F, G) + H (contains "t1" SHA-256 variable)
 
                 add s3, s4, a5  // s3 now contains new E. At this point state variables H, G, F and E have been updated
-                
+
                 // Updates D, C, and B variables, freeing up s7 (current A) register for temp use
                 c.mv s4, s5  // s4 now contains C
                 c.mv s5, s6  // s5 now contains B
@@ -278,8 +278,8 @@ void sha256_compress(char* state, char* data) {
                 add s7, a2, a5  // A <= "t1" + "t2", this finishes updating all state vars for this iteration
 
             // Increment counters and loop back to top_iter
-            c.addi t0, 1
-            c.addi sp, 1
+            c.addi t0, 4
+            c.addi sp, 4
             slti a3, t0, 64
             c.bnez sha256_compress_iter_top
 
@@ -292,7 +292,7 @@ void sha256_compress(char* state, char* data) {
         c.add s11, s5
         c.add a6, s6
         c.add a7, s7
-        
+
     )
 }
 
@@ -311,7 +311,7 @@ void sha256_finish(char* state) {
         c.swsp s11, 5
         c.swsp a6, 6
         c.swsp a7, 7
-    
+
         // Restore stack
         c.mv t2, sp
         c.lwsp s0, 0
