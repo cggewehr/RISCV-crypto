152e052bd805adacb22eb7d4bef56913cfdca038
Carlos Gewehr <carlos.gewehr@paxos.portoalegre.pucrsnet.br>
2022-11-24 23:14:41 -0300

    RTL cleanup after SHA256 RTL implementation name changes

--- src/rtl/ibex-crypto/crypto_sha2_unit_pkg.sv
+++
@@ -1,7 +0,0 @@
-package sha2_pkg;
-
-  `define ROTR(x, n) ($bits(x)'(x >> n) | $bits(x)'(x << 32 - n))
-
-  typedef enum {SIG0, SIG1, SUM0, SUM1, SIG0H, SIG0L, SIG1H, SIG1L, SUM0R, SUM1R} sha2_op_t;
-
-endpackage
