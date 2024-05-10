c76193af0321eed25f81d74ac962fbe2492dd595
Carlos Gewehr <cggewehr@gmail.com>
2024-05-09 23:46:04 -0300

    Use QRC file for better interconnect delay estimation

--- scripts/genus/power_analysis.tcl
+++ scripts/genus/power_analysis.tcl
@@ -11,10 +11,14 @@ read_db ${DB_FILE}
 #set_db library "/soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_12_CORE_LR@2.0@20130411.0/libs/C28SOI_SC_12_CORE_LR_tt28_0.90V_25C.lib \
 #                /soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_12_PR_LR@2.0@20130412.0/libs/C28SOI_SC_12_PR_LR_tt28_0.90V_25C.lib \
 #                /soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_12_CLK_LR@2.1@20130621.0/libs/C28SOI_SC_12_CLK_LR_tt28_0.90V_25C.lib"
-             
+
 set_db library "/soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_8_CORE_LL@2.1@20131011.0/libs/C28SOI_SC_8_CORE_LL_tt28_0.90V_0.00V_0.00V_0.00V_25C.lib \
                 /soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_8_PR_LL@2.1@20131028.1/libs/C28SOI_SC_8_PR_LL_tt28_0.90V_0.00V_0.00V_0.00V_25C.lib \
                 /soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_8_CLK_LL@2.2@20131011.0/libs/C28SOI_SC_8_CLK_LL_tt28_0.90V_0.00V_0.00V_0.00V_25C.lib"
+
+set_db qrc_tech_file "/soft64/design-kits/stm/28nm-cmos28lp_42/CadenceTechnoKit_cmos028_6U1x_2U2x_2T8x_LB@4.2.1/QRC_TECHFILE/nominal.tech"
+
+set_db interconnect_mode ple
 
 read_stimulus ${SHM_FILE} -dut_instance /tb_top/u_ibex_simple_system/u_top/u_ibex_top -start ${START_TIME} -end ${END_TIME}
 compute_power -mode average
--- scripts/genus/setup.tcl
+++ scripts/genus/setup.tcl
@@ -47,7 +47,7 @@ set_db lef_library "/soft64/design-kits/stm/28nm-cmos28fdsoi_25d/SiteDefKit_cmos
                     /soft64/design-kits/stm/28nm-cmos28fdsoi_25d/C28SOI_SC_8_CLK_LL@2.2@20131011.0/CADENCE/LEF/C28SOI_SC_8_CLK_LL_soc.lef"
 
 # Set captable
-set_db cap_table_file "/soft64/design-kits/stm/28nm-cmos28lp_42/CadenceTechnoKit_cmos028_6U1x_2U2x_2T8x_LB@4.2.1/CAP_TABLE/FuncRCmax.captable"
+#set_db cap_table_file "/soft64/design-kits/stm/28nm-cmos28lp_42/CadenceTechnoKit_cmos028_6U1x_2U2x_2T8x_LB@4.2.1/CAP_TABLE/FuncRCmax.captable"
 set_db qrc_tech_file "/soft64/design-kits/stm/28nm-cmos28lp_42/CadenceTechnoKit_cmos028_6U1x_2U2x_2T8x_LB@4.2.1/QRC_TECHFILE/FuncRCmax.tech"
 
 # Set PLE
